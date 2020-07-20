c7c1e90f1d60be70a271482492526016ec3f2b10
Kevin Rosado <kevin.rosado.s@uni.pe>
2020-07-20 16:40:14 -0500

    avance2

--- tiendaweb/src/main/java/com/tiendavideojuegos/tiendaweb/dao/TiendaDaoImpl.java
+++ tiendaweb/src/main/java/com/tiendavideojuegos/tiendaweb/dao/TiendaDaoImpl.java
@@ -1,14 +1,34 @@
 package com.tiendavideojuegos.tiendaweb.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.tiendavideojuegos.tiendaweb.dto.TiendaDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class TiendaDaoImpl implements CrudDaoInterface<TiendaDto> {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     public List<TiendaDto> getAll() {
-      //Tenemos que hacer esto ununununu
-        return null;
+      List<TiendaDto> dtoList = new ArrayList<>();
+      String sql = "SELECT nombretienda, idtienda FROM tienda";
+      try{
+          Connection cn = jdbcTemplate.getDataSource().getConnection();
+          Statement sentencia = cn.createStatement();
+          ResultSet rs = sentencia.executeQuery(sql);
+          while(rs.next()){
+              TiendaDto tiendaDto = new TiendaDto();
+          }
+      }catch (SQLException throwables){
+          throwables.printStackTrace();
+      }
+        return dtoList;
     }
 
 
