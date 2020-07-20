b038b2e882748153629feee59fc5a7a82f472f29
Kevin Rosado <kevin.rosado.s@uni.pe>
2020-07-20 17:13:07 -0500

    relaciontv

--- tiendaweb/src/main/java/com/tiendavideojuegos/tiendaweb/dao/RelacionTvDaoImpl.java
+++ tiendaweb/src/main/java/com/tiendavideojuegos/tiendaweb/dao/RelacionTvDaoImpl.java
@@ -46,12 +46,34 @@ public class RelacionTvDaoImpl implements CrudDaoInterface<RelacionTv> {
         }
         return lista;
     }
-    
 
-    
-    public List<RelacionTv> getbyId(float num) {
+    @Autowired
+    public List<RelacionTv> getbyId(String id) {
+        String sql = "SELECT idtienda, idvideojuego, urltv, precioNormal, precioOferta, DiaFinOferta " +
+                "FROM relaciontv WHERE idvideojuego = ?";
+        List<RelacionTv> lista = new ArrayList<RelacionTv>();
+        try{
+            Connection cn = jdbcTemplate.getDataSource().getConnection();
+            Statement sentencia = cn.createStatement();
+            ResultSet resultado = sentencia.executeQuery(sql);
+            while(resultado.next()){
+                RelacionTv relacionTv= new RelacionTv();
+                relacionTv.setIDTienda(resultado.getString("idtienda"));
+                relacionTv.setIDvideojuego(resultado.getString(id));
+                relacionTv.setUrlTV(resultado.getString("urltv"));
+                Date dateObj = resultado.getDate("DiaFinOferta");
+                //Converting the Date object to String format
+                String date = dateObj.toString();
+                relacionTv.setDiaFinOferta(date);
+                relacionTv.setPrecioNormal(resultado.getDouble("precioNormal"));
+                relacionTv.setPrecioOferta(resultado.getDouble("precioOferta"));
+                lista.add(relacionTv);
+            }
 
-        return null;
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+        return lista;
     }
     
     
